d44be027e94007ae269143602272bef6051592b2
dosaem90 <dosaem90@gmail.com>
2019-08-05 21:32:50 +0900

    Updates

---
+++ precache-manifest.803648f7b8d4940a39680ad06883697b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8902d3f1ddd4fa593a4a073d31a4dcd3",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "72e130ff510a2583cb0e",
+    "url": "/react_movie_app/static/css/main.30aee697.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/react_movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "72e130ff510a2583cb0e",
+    "url": "/react_movie_app/static/js/main.c834f4e2.chunk.js"
+  },
+  {
+    "revision": "faf18139e1d65c6c220e",
+    "url": "/react_movie_app/static/js/runtime~main.314610f3.js"
+  }
+]);
